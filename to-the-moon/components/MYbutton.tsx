1cd6ca52c8cb85b2470e5bf557373c621b9613ef
Moo02 <mjung1541@gmail.com>
2023-02-26 15:34:03 -0600

    mybutton

---
+++ to-the-moon/components/MYbutton.tsx
@@ -0,0 +1,12 @@
+import styles from "./MYbutton.module.css";
+
+import { useEffect, useState } from "react";
+
+export default function MYbutton() {
+
+  return (
+    <div className={styles.main}>
+        <h4>Button</h4>
+    </div>
+  );
+}
