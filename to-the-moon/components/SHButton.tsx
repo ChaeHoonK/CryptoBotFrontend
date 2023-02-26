846e0329f14006f37ac2b68304847b2b6018fd83
Chae <114709354+ChaeHoonK@users.noreply.github.com>
2023-02-26 15:48:25 -0600

    component attribute

--- to-the-moon/components/SHButton.tsx
+++ to-the-moon/components/SHButton.tsx
@@ -1,6 +1,6 @@
 import styles from "./SHButton.module.css";
 
-export default function SHButton() {
+export default function SHButton({price} : {price? : number}) {
 
     function handleClick() {
         const ans = prompt("how old are you?")
@@ -9,6 +9,7 @@ export default function SHButton() {
   return (
     <div className={styles.main} onClick = {handleClick}>
       <h4>Button</h4>
+      <h1>{price}</h1>
     </div>
   );
 }
--- to-the-moon/pages/chaehoon.tsx
+++ to-the-moon/pages/chaehoon.tsx
@@ -1,6 +1,7 @@
 import { Inter } from "@next/font/google";
 import styles from "@/styles/chaehoon.module.css";
 import CHButton from '../components/CHButton'
+import SHButton from "@/components/SHButton";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -12,6 +13,7 @@ export default function Home() {
       <CHButton />
       <CHButton />
       <CHButton />
+      <SHButton price = {150}/>
     </div>
   );
 }
