60f86be2599a87cb68087ebcdcdf89aeb252d3a7
Chae <114709354+ChaeHoonK@users.noreply.github.com>
2023-04-24 14:15:42 -0500

    analytics added

--- to-the-moon/pages/_app.tsx
+++ to-the-moon/pages/_app.tsx
@@ -5,12 +5,14 @@ import { ChatProvider } from "../context/ChatContext";
 import { WalletProvider } from "../context/WalletContext";
 import React from "react";
 import "react-datepicker/dist/react-datepicker.css";
+import { Analytics } from "@vercel/analytics/react";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <WalletProvider>
       <ChatProvider>
         <Component {...pageProps} />
+        <Analytics />
       </ChatProvider>
     </WalletProvider>
   );
